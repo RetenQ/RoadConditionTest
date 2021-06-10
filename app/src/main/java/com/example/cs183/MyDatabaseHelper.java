d071b2cd13d432a49d82991ba903dbfecd80d5ff
RetenQ <18965093000@163.com>
2021-06-10 20:33:15 +0800

    增加MyDatabaseHelper

---
+++ app/src/main/java/com/example/cs183/MyDatabaseHelper.java
@@ -0,0 +1,43 @@
+package com.example.cs183;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    //创建表 并声明类型
+    public static final String CREATE_BOOK = "create table HoleDatabase1("
+
+            +"rank integer,"
+            +"time long,"
+            +"value float,"
+            +"Latitude double,"
+            +"Longitude double)";
+
+
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+        super(context,name,factory,version);
+        mContext = context;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(CREATE_BOOK);
+        Toast.makeText(mContext,"Create succeeded",Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
+
+    }
+
+
+
+
+
+}
+
+
+
